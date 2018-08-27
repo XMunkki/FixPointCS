66fa3927c2b0f721974d3c14c8d054191af70ab4
Petri Kero <petrikero2@gmail.com>
2018-08-27 22:24:31 +0300

    Add missing Transpiler files. Remove unused FPBigInt.cs.

---
+++ Transpiler/GenerateJava.cs
@@ -0,0 +1,89 @@
+//
+// FixPointCS
+//
+// Copyright(c) 2018 Jere Sanisalo, Petri Kero
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Transpiler
+{
+    public static class GenerateJava
+    {
+        public static void ConvertFile(string inPath, string outPath)
+        {
+            Console.WriteLine("Generating {0}..", outPath);
+
+            // Read the file
+            string[] lines = File.ReadAllLines(inPath);
+
+            // Preprocess
+            lines = Util.Preprocess(lines, "JAVA");
+            lines = Util.Unindent(lines, 1);
+
+            // Prepare output
+            StringBuilder sb = new StringBuilder();
+
+            // \todo [petri] actual conversion!
+            foreach (string lineIn in lines)
+            {
+                string line = lineIn;
+                string trimmed = line.Trim();
+
+                // Ignore [...] -lines (attributes)
+                if (trimmed.StartsWith("["))
+                    continue;
+
+                // Process line
+                line = Util.ReplaceWholeWord(line, "// PREFIX", g_prefix);
+                line = Util.ReplaceWholeWord(line, "// SUFFIX", g_suffix);
+                line = Util.ReplaceWholeWord(line, "static class", "class");                    // static classes
+                line = Util.ReplaceWholeWord(line, "static readonly", "static final");          // const arrays
+                line = Util.ReplaceWholeWord(line, "public const", "public static final");      // global variables
+                line = Util.ReplaceWholeWord(line, "private const", "private static final");    // global variables
+                line = Util.ReplaceWholeWord(line, "const", "final");                           // method-local constants
+                line = Util.ReplaceWholeWord(line, "Debug.Assert", "assert");
+                line = line.Replace("Nlz((ulong)", "Nlz(");
+                line = line.Replace("Nlz((uint)", "Nlz(");
+
+                sb.AppendLine(line);
+            }
+
+            // Save the file
+            File.WriteAllText(outPath, sb.ToString(), Encoding.ASCII);
+        }
+
+        // Prefix for the generated file
+        private static string g_prefix = @"//
+// GENERATED FILE!!!
+//
+// Generated from Fixed64.cs, part of the FixPointCS project (MIT license).
+//
+";
+
+        private static string g_suffix = @"// END GENERATED FILE";
+    }
+}
---
+++ Transpiler/Util.cs
@@ -0,0 +1,160 @@
+//
+// FixPointCS
+//
+// Copyright(c) 2018 Jere Sanisalo, Petri Kero
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Transpiler
+{
+    public static class Util
+    {
+        public static string[] Preprocess(string[] lines, string languageDefine)
+        {
+            List<string> output = new List<string>();
+
+            // Process the lines
+            bool included = true;
+            bool langChecked = false;
+            foreach (string line in lines)
+            {
+                // Poor mans preprocessor handling
+                if (line.StartsWith("#"))
+                {
+                    Debug.Assert(line == line.Trim(), "Must not have whitespace on preprocessor lines");
+                    if (line == $"#if {languageDefine}" || line == $"#elif {languageDefine}")
+                    {
+                        included = true; // matching language block
+                        langChecked = true;
+                    }
+                    else if (line.StartsWith("#if ") || line.StartsWith("#elif "))
+                        included = false; // non-matching language block
+                    else if (line == "#if !TRANSPILE")
+                        included = false;
+                    else if (line.StartsWith("#else"))
+                        included = !langChecked;
+                    else if (line.StartsWith("#endif"))
+                    {
+                        included = true;
+                        langChecked = false;
+                    }
+                    else
+                        Debug.Fail("Unrecognized preprocessor line: '{0}'", line);
+
+                    continue;
+                }
+
+                if (!included)
+                    continue;
+
+                output.Add(line);
+            }
+
+            return output.ToArray();
+        }
+
+        public static string[] Unindent(string[] lines, int amount)
+        {
+            return lines.Select(line =>
+            {
+                for (int i = 0; i < amount; i++)
+                {
+                    if (line.StartsWith("    "))
+                        line = line.Substring(4);
+                    else if (line.StartsWith("\t"))
+                        line = line.Substring(1);
+                }
+
+                return line;
+            }).ToArray();
+        }
+
+        // Replaces all occurances of the whole word (in the C# sense) with the given text
+        public static string ReplaceWholeWord(string str, string old_word, string new_word)
+        {
+            int old_len = old_word.Length;
+
+            // Early exits
+            if (old_len == 0)
+                return str;
+            if (!str.Contains(old_word))
+                return str;
+
+            StringBuilder sb = new StringBuilder();
+            int search_pos = 0;
+
+            while (true)
+            {
+                // Find the next hit
+                search_pos = Math.Min(search_pos, str.Length); // Clamp the search area
+                int next_hit = str.IndexOf(old_word, search_pos);
+
+                // No more hits?
+                if (next_hit == -1)
+                {
+                    sb.Append(str.Substring(search_pos));
+                    return sb.ToString();
+                }
+
+                // Copy to the hit
+                if (next_hit > search_pos)
+                {
+                    sb.Append(str.Substring(search_pos, next_hit - search_pos));
+                    search_pos = next_hit;
+                }
+
+                // Check if the hit is on a word boundary
+                bool is_ws_start = (next_hit == 0) || (IsWordBoundary(str[next_hit - 1]));
+                bool is_ws_end = ((next_hit + old_len) == str.Length) || (IsWordBoundary(str[next_hit + old_len]));
+
+                if (is_ws_start && is_ws_end)
+                { // On a word boundary
+                    if (new_word.Length > 0)
+                    { // Fit the new word in
+                        sb.Append(new_word);
+                    }
+                    else
+                    { // Replacing with empty; skip the next whitespace in the source
+                        search_pos += 1;
+                    }
+                }
+                else
+                { // Not on a word boundary
+                    sb.Append(str.Substring(next_hit, old_len));
+                }
+                search_pos += old_len;
+            }
+        }
+
+        // Tests if the character is a valid C# identifier boundary character
+        private static bool IsWordBoundary(char c)
+        {
+            if (Char.IsLetterOrDigit(c) || c == '_')
+                return false;
+            return true;
+        }
+    }
+}
